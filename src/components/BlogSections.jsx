ffe9d28c0bd2110a23c7747c6a1f1c97f13ce344
LearnLikeLeon <leon@learnlikeleon.com>
2024-08-01 20:58:22 -0400

    Edited Portfolio images size

--- src/components/BlogSections.jsx
+++ src/components/BlogSections.jsx
@@ -174,8 +174,8 @@ export default function BlogSections() {
                   className="rounded-2xl drop-shadow-md hover:drop-shadow-xl transition-all duration-200 delay-100"
                   src={`${post.projectImageUrl}`}
                   alt="An image of the final project"
-                  width={50}
-                  height={50}
+                  width={250}
+                  height={200}
                 />
               </div>
 
