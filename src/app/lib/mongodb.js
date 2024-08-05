7526685faf327eedf5b7a7067529bcdda3593431
LearnLikeLeon <leon@learnlikeleon.com>
2024-08-05 08:19:15 -0400

    private url endpoint edited

--- src/app/lib/mongodb.js
+++ src/app/lib/mongodb.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     if (mongoose.connection.readyState === 0) {
-      await mongoose.connect(process.env.DATABASE_URL);
-      console.log("db connected");
+      await mongoose.connect(process.env.REACT_APP_DATABASE_URL);
+      console.log("database successfully connected");
     }
   } catch (error) {
     console.log(error);
